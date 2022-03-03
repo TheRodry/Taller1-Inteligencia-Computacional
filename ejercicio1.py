0fb47aa85491a4ab06ae148bafb78959b0e9535e
TheRodry <rohesa09@gmail.com>
2022-03-02 21:09:46 -0500

    modificacion ejercicio uno

--- ejercicio1.py
+++ ejercicio1.py
@@ -5,3 +5,16 @@ Spyder Editor
 This is a temporary script file.
 """
 
+donacion=int(input("ingrese el monto de la donacion"))
+tel=donacion*0.1
+sis=donacion*0.25
+admi=donacion*0.35
+cont=donacion*0.30
+
+print(f"el valor de la donacion es igual a : ${donacion:,}")
+print(f"el valor de la donacion correspondiente a las telecomunicaciones es: ${tel:,}")
+print(f"el valor de la donacion correspondiente  a sistemas es  : ${sis:,}")
+print(f"el valor de la donacion correspondiente a la administracion es  : ${admi:,}")
+print(f"el valor de la donacion correspondiente a contabilidad es  : ${cont:,}")
+
+
