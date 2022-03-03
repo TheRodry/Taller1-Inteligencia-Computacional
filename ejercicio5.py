8b20fc67ce6277fca6196508002cfc608c63a433
TheRodry <rohesa09@gmail.com>
2022-03-02 20:49:22 -0500

    quinto ejercicio

---
+++ ejercicio5.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  2 20:21:42 2022
+
+@author: rodri
+"""
+
+Alumnosr=int(input("digite lña cantidad alumnos en la clase de redes: "))
+Alumnosc=int(input("digite lña cantidad alumnos en la clase de contabilidad: "))
+Alumnosd=int(input("digite lña cantidad alumnos en la clase de diseño: "))
+
+totala=Alumnosr+Alumnosc+Alumnosd
+
+porR=(Alumnosr/totala)*100
+porC=(Alumnosc/totala)*100
+porD=(Alumnosd/totala)*100
+
+print(f"el porcentaje de los alumnos de redes es  : {porR}%")
+print(f"el porcentaje de los alumnos de contabilidad es  : {porC}%")
+print(f"el porcentaje de los alumnos de diseño es  : {porD}%")
